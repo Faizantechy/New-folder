7b5560573681954abbaa5870515ebbfa4a9e972c
FaizanTechy <faizanraza112112@gmail.com>
2024-12-09 10:41:35 +0500

    commits

---
+++ my-Application/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+
+        <h1>Are you there mate?</h1>
+        
+      </div>
+      
+    </>
+  )
+}
+
+export default App
